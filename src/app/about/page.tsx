07d4f1b4084b68ddca5287f96dcec7799c63799d
Bernardo <bernardojavier18@gmail.com>
2025-03-22 01:00:43 -0400

    Make initial common parser component (basic POC)

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import {ParseContent} from '@/app/components/parse-content'
+
+
+
+export default function About() {
+    return (
+        <div>
+            <h1>Example file data:</h1>
+            <ParseContent />
+        </div>
+    )
+}
