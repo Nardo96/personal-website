f193a5e89424ff9b61000533db6f6172c3686c1f
Bernardo <bernardojavier18@gmail.com>
2025-03-23 17:42:35 -0400

    Refactor slightly to incorporate children prop passing pattern

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,9 +5,19 @@ import PageParse from '@/app/components/page-parse'
 export default function About() {
     console.log(blocks_about)
     return (
-        <div>
+        <>
             <h1>Example file data:</h1>
-            <PageParse blocks={blocks_about} mapper="about"/>
+            <ContentContainer>
+                <PageParse blocks={blocks_about} mapper="about"/>
+            </ContentContainer>
+        </>
+    )
+}
+
+function ContentContainer({children}) {
+    return (
+        <div className='ContentContainer'>
+            {children}
         </div>
     )
 }
