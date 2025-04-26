98bb24e47b2ec7bc69d90c62908bf3d48192fa30
Bernardo <bernardojavier18@gmail.com>
2025-04-26 19:51:19 -0400

    Update root layout -nav bar + grid center main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           <a href="/translations/">J-E Translations</a>
           <a href="/projects/">Code Projects</a>
         </nav>
-        <main>{children}</main>
+        <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">{children}</main>
       </body>
     </html>
   );
