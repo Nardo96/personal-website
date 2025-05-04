fed3bd3c3bf72aa124b04f77a4781f50f1c024f0
Bernardo <bernardojavier18@gmail.com>
2025-05-04 09:52:48 -0400

    Update styling

--- src/app/components/page-parse.tsx
+++ src/app/components/page-parse.tsx
@@ -3,10 +3,10 @@ import Block from "@/app/components/block"
 export default function PageParse({blocks, mapper}) {
     console.log(`Parsing ${mapper}`)
     return (
-        <>
+        <div className="w-full flex flex-col gap-8">
         {blocks.map((block,index) => (
             <Block block={block} key={index} mapperString={mapper}></Block>
         ))}
-        </>
+        </div>
     )
 }
