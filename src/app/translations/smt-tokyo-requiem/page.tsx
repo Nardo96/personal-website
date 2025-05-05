d78aa5ddfccd75ce890190cdcd86195938d1ca8b
Bernardo <bernardojavier18@gmail.com>
2025-05-05 14:15:01 -0400

    Small text update

--- src/app/translations/smt-tokyo-requiem/page.tsx
+++ src/app/translations/smt-tokyo-requiem/page.tsx
@@ -5,8 +5,8 @@ export default function BookPage() {
             <h1 className="text-center font-bold text-2xl mb-4">Shin Megami Tensei: Tokyo Requiem</h1>
             <Image src="/TokyoRequiem_title.webp" alt="Tokyo Requiem Title Screen (source: https://megamitensei.fandom.com/wiki/Shin_Megami_Tensei:_Tokyo_Requiem?file=TokyoRequiem_Title.jpg)" width={500} height={500} className="mx-auto mb-4"/>
             <div className="flex flex-col gap-4">
-                <a href="/translations/smt-tokyo-requiem/prologue-site-version">Prologue: Site Version</a>
-                <a href="/translations/smt-tokyo-requiem/prologue-game-version">Prologue: Game Version</a>
+                <a href="/translations/smt-tokyo-requiem/prologue-site-version">Prologue - Site Version</a>
+                <a href="/translations/smt-tokyo-requiem/prologue-game-version">Prologue - Game Version</a>
                 <a href="/translations/smt-tokyo-requiem/opening">Opening</a>
                 <a href="/translations/smt-tokyo-requiem/chapter-1">Chapter 1</a>
                 <a href="/translations/smt-tokyo-requiem/chapter-2">Chapter 2</a>
