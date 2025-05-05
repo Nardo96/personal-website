6c966c702efe9e4afaa20e2906a4de926dd6c6f2
Bernardo <bernardojavier18@gmail.com>
2025-05-05 13:08:16 -0400

    Small spacing styling update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
         <>
             <h1 className="font-bold text-2xl text-center mb-4">Main Projects</h1>
             <PageParse blocks={blocks_projects} mapper="projects"/>
-            <h1 className="font-bold text-1.5xl text-center">Visit my github profile for other small projects!</h1>
+            <h1 className="font-bold text-1.5xl text-center mt-4">Visit my github profile for other small projects!</h1>
             <h1 className="text-center -mt-16">https://github.com/Nardo96/</h1>
         </>
     )
