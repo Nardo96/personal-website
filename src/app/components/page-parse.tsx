281991bc52ef8ab30c38d91c0b8f66de31a005da
Bernardo <bernardojavier18@gmail.com>
2025-05-06 22:35:41 -0400

    Refactor page parse. Mapper will determine styling using in div that contains the block contents

--- src/app/components/page-parse.tsx
+++ src/app/components/page-parse.tsx
@@ -2,38 +2,18 @@ import Block from "@/app/components/block"
 
 export default function PageParse({blocks, mapper}) {
     console.log(`Parsing ${mapper}`)
-    if (mapper == "projects") {
-        return (
-            <>
-            {blocks.map((block,index) => (
-                <div key={index}>
-                    <Block block={block} key={index} mapperString={mapper}></Block>
-                </div>
-            ))}
-            </>
-        )
+    let styling = "";
+    if (mapper == "about-skills") {
+        styling = "border rounded-md border-white justify-between flex flex-col p-3 w-1/3 min-w-60"
     }
-    else if (mapper == "translations") {
-        return (
-            <>
-            {blocks.map((block,index) => (
-                <div key={index}>
-                    <Block block={block} key={index} mapperString={mapper}></Block>
-                </div>
-            ))}
-            </>
-        )
-    }
-    else {
-        return (
-            <>
-            {blocks.map((block,index) => (
-                <div key={index}>
-                    <Block block={block} key={index} mapperString={mapper}></Block>
-                </div>
-                
-            ))}
-            </>
-        )
-    }
+    return (
+        <>
+        {blocks.map((block,index) => (
+            <div key={index} className={styling}>
+                <Block block={block} key={index} mapperString={mapper}></Block>
+            </div>
+            
+        ))}
+        </>
+    )
 }
